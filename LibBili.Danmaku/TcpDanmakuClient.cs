97ed5c4151e876a8ab4f6defdb1fae632303fc6a
shabby2333 <1308933842@qq.com>
2021-10-30 00:37:10 +0800

    Fix code style

--- LibBili.Danmaku/TcpDanmakuClient.cs
+++ LibBili.Danmaku/TcpDanmakuClient.cs
@@ -1,32 +1,27 @@
 ﻿using System;
 using System.Buffers;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
 
 namespace LibBili.Danmaku
 {
     public class TcpDanmakuClient : IBiliDanmakuClient
     {
         private const int TCP_PORT = 2243;
+        private const string DEFAULT_DANMAKU_URL = "hw-bj-live-comet-05.chat.bilibili.com";
 
-        private TcpClient _socket; 
-        private string _url = "hw-bj-live-comet-05.chat.bilibili.com";
+        private TcpClient _socket;
+        private string _url;
         private int _port = TCP_PORT;
 
-        public TcpDanmakuClient(long roomID) : this(roomID, null) { }
-        public TcpDanmakuClient(long roomID, long? realRoomID) : base(roomID, realRoomID) {  }
-
-
-        public async override void Connect()
+        public TcpDanmakuClient(long roomID, long? realRoomID = null) : base(roomID, realRoomID)
         {
-            if(_socket is not null)
+        }
+
+        public override async void Connect()
+        {
+            if (_socket is not null)
             {
                 _socket?.Close();
                 _socket.Dispose();
@@ -38,7 +33,7 @@ namespace LibBili.Danmaku
                 if (RoomID < 10000)
                 {
                     var resp = await GetRoomInfoAsync(RoomID);
-                    RealRoomID = (long)resp["room_info"]["room_id"];
+                    RealRoomID = (long)resp["room_info"]?["room_id"];
                 }
                 else
                     RealRoomID = RoomID;
@@ -46,16 +41,16 @@ namespace LibBili.Danmaku
 
             //根据房间号获取弹幕服务器地址信息及验证信息
             var info = await GetDanmakuLinkInfoAsync(RealRoomID.Value);
-            _url = info["host_list"][0]["host"].ToString();
-            _port = info["host_list"][0]["port"].ToObject<int>();
-            _token = info["token"].ToString();
+            _url = info["host_list"]?[0]?["host"]?.ToString() ?? DEFAULT_DANMAKU_URL;
+            _port = info["host_list"]?[0]?["port"]?.ToObject<int?>() ?? TCP_PORT;
+            _token = info["token"]?.ToString();
 
             _socket = new TcpClient();
-            _socket.Connect(_url, _port);
-            if(_socket.Connected)
+            await _socket.ConnectAsync(_url, _port);
+            if (_socket.Connected)
                 OnOpen();
 
-            while(_socket is not null && _socket.Connected)
+            while (_socket is not null && _socket.Connected)
             {
                 var bytes = ArrayPool<byte>.Shared.Rent(_socket.ReceiveBufferSize);
                 var ms = new MemoryStream();
@@ -65,11 +60,11 @@ namespace LibBili.Danmaku
                     len = await _socket.GetStream().ReadAsync(bytes);
                     ms.Write(bytes);
                 } while (len >= bytes.Length);
+
                 ArrayPool<byte>.Shared.Return(bytes);
 
                 ProcessPacket(ms.ToArray().AsSpan());
             }
-            
         }
 
         public override void Disconnect()
@@ -85,10 +80,9 @@ namespace LibBili.Danmaku
             GC.SuppressFinalize(this);
         }
 
-        public override void Send(byte[] packet) => _socket?.GetStream()?.Write(packet);
+        public override void Send(byte[] packet) => _socket?.GetStream().Write(packet);
         public override void Send(Packet packet) => Send(packet.ToBytes);
         public override async Task SendAsync(byte[] packet) => await _socket.GetStream().WriteAsync(packet);
         public override Task SendAsync(Packet packet) => SendAsync(packet.ToBytes);
-
     }
 }
--- LibBili.Danmaku/WebsocketDanmakuClient.cs
+++ LibBili.Danmaku/WebsocketDanmakuClient.cs
@@ -1,13 +1,5 @@
-﻿using LibBili.Api.Util;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System;
 using System.Net.WebSockets;
-using System.Text;
 using System.Threading.Tasks;
 using Websocket.Client;
 
@@ -15,23 +7,31 @@ namespace LibBili.Danmaku
 {
     public class WebsocketDanmakuClient : IBiliDanmakuClient
     {
+        private const int WS_PORT = 2244;
+        private const int WSS_PORT = 443;
+        private const string DEFAULT_DANMAKU_URL = "hw-bj-live-comet-05.chat.bilibili.com";
+
+
         private WebsocketClient _ws;
         private string _url = "wss://hw-bj-live-comet-05.chat.bilibili.com/sub";
 
-        public WebsocketDanmakuClient(long roomID) : this(roomID, null) { }
-        public WebsocketDanmakuClient(long roomID, long? realRoomID) : base(roomID, realRoomID){  }
+        public WebsocketDanmakuClient(long roomID, long? realRoomID = null) : base(roomID, realRoomID)
+        {
+        }
 
-        public async override void Connect()
+        public override async void Connect()
         {
             //尝试释放已连接的ws
             _ws?.Stop(WebSocketCloseStatus.Empty, string.Empty);
             _ws?.Dispose();
 
-            if(!RealRoomID.HasValue){
+            if (!RealRoomID.HasValue)
+            {
+                // 汪姐姐说短房间号只有10000以下的，出问题记得点艹他
                 if (RoomID < 10000)
                 {
                     var resp = await GetRoomInfoAsync(RoomID);
-                    RealRoomID = (long)resp["room_info"]["room_id"];
+                    RealRoomID = (long)resp["room_info"]?["room_id"];
                 }
                 else
                     RealRoomID = RoomID;
@@ -39,24 +39,24 @@ namespace LibBili.Danmaku
 
             //根据房间号获取弹幕服务器地址信息及验证信息
             var info = await GetDanmakuLinkInfoAsync(RealRoomID.Value);
-            _url = $"wss://{info["host_list"][0]["host"]}:{info["host_list"][0]["wss_port"]}/sub";
-            _token = info["token"].ToString();
+            _url =
+                $"wss://{info["host_list"]?[0]?["host"] ?? DEFAULT_DANMAKU_URL}:{info["host_list"]?[0]?["wss_port"] ?? WSS_PORT}/sub";
+            _token = info["token"]?.ToString();
             _ws = new WebsocketClient(new Uri(_url), () => new ClientWebSocket { Options = { Cookies = _cookies } });
 
             _ws.MessageReceived.Subscribe(e => ProcessPacket(e.Binary));
             //TODO: 关闭及异常事件处理
             _ws.DisconnectionHappened.Subscribe(e =>
             {
-                if (e.CloseStatus == WebSocketCloseStatus.Empty) 
+                if (e.CloseStatus == WebSocketCloseStatus.Empty)
                     Console.WriteLine("WS CLOSED");
                 else
-                    Console.WriteLine("WS ERROR: " + e.Exception.Message); 
+                    Console.WriteLine("WS ERROR: " + e.Exception.Message);
                 Connected = false;
-
             });
             await _ws.Start();
             // 如果成功连接ws 触发onopen事件，发送初始包
-            if(_ws.IsStarted)
+            if (_ws.IsStarted)
                 OnOpen();
         }
 
@@ -77,9 +77,5 @@ namespace LibBili.Danmaku
         public override void Send(Packet packet) => Send(packet.ToBytes);
         public override Task SendAsync(byte[] packet) => Task.Run(() => Send(packet));
         public override Task SendAsync(Packet packet) => SendAsync(packet.ToBytes);
-
-        
-
-        
     }
 }
