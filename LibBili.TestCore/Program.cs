4d4396bdd1ba0fc4f350a6a1d6948103ac1d6217
shabby2333 <1308933842@qq.com>
2022-05-02 23:13:49 +0800

    add: new ws danmaku packet type

--- LibBili.TestCore/Program.cs
+++ LibBili.TestCore/Program.cs
@@ -5,9 +5,11 @@ Console.Write("请输入房间号：");
 var roomId = Convert.ToInt64(Console.ReadLine());
 var ws = new WebsocketDanmakuClient(roomId);
 ws.UpdatePopularity += (sender, e) => Console.Title = $"热度: {e}";
+ws.UpdateWatched += (sender, e) => Console.Title = $"{e} 人观看过";
 ws.ReceiveDanmaku += (sender, e) =>
-    Console.WriteLine($"{DateTime.Now}[{(((BiliDanmakuClient)sender)!).RoomID}]  {(e.Danmaku.IsAdmin ? "[房]" : "")}{(e.Danmaku.IsVIP ? "[爷]" : "")}{e.Danmaku.UserName}: {e.Danmaku.Text}");
-ws.ReceiveGift += (sender, e) => Console.WriteLine($"{DateTime.Now}[{(((BiliDanmakuClient)sender)!).RoomID}]  {e.Gift.UserName} 赠送 {e.Gift.GiftName}*{e.Gift.GiftCount}");
+    Console.WriteLine($"{DateTime.Now:HH:mm:ss}[{(((BiliDanmakuClient)sender)!).RoomID}][弹幕] {(e.Danmaku.IsAdmin ? "[房]" : "")}{(e.Danmaku.IsVIP ? "[爷]" : "")}{e.Danmaku.UserName}: {e.Danmaku.Text}");
+ws.ReceiveGift += (sender, e) => 
+    Console.WriteLine($"{DateTime.Now:HH:mm:ss}[{(((BiliDanmakuClient)sender)!).RoomID}][礼物] {e.Gift.UserName} 赠送 {e.Gift.GiftName}*{e.Gift.GiftCount}");
 ws.Connect();
 
-Console.ReadLine();
+_ = Console.ReadLine();
